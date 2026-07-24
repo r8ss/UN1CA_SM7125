840478677d9fc0ee4c1d814c665dbaa0a334f95d
redmax19 <alessandroaraujo0098@gmail.com>
2026-07-24 16:59:51 +0000

    scripts

--- unica/mods/knoxpatch/customize.sh
+++ unica/mods/knoxpatch/customize.sh
@@ -8,18 +8,21 @@ DELETE_FROM_WORK_DIR "system" "system/lib64/vendor.samsung.hardware.security.wsm
 # Add KnoxPatchHooks
 APPLY_PATCH "system" "system/framework/framework.jar" \
     "$MODPATH/framework.jar/0001-Introduce-KnoxPatchHooks.patch"
+
 SMALI_PATCH "system" "system/framework/framework.jar" \
     "smali/android/app/Instrumentation.smali" "replace" \
     'newApplication(Ljava/lang/Class;Landroid/content/Context;)Landroid/app/Application;' \
     'return-object p0' \
     '    invoke-static {p1}, Lio/mesalabs/unica/KnoxPatchHooks;->init(Landroid/content/Context;)V\n\n    return-object p0' \
     > /dev/null
+
 SMALI_PATCH "system" "system/framework/framework.jar" \
     "smali/android/app/Instrumentation.smali" "replace" \
     'newApplication(Ljava/lang/ClassLoader;Ljava/lang/String;Landroid/content/Context;)Landroid/app/Application;' \
     'return-object p0' \
     '    invoke-static {p3}, Lio/mesalabs/unica/KnoxPatchHooks;->init(Landroid/content/Context;)V\n\n    return-object p0' \
     > /dev/null
+
 APPLY_PATCH "system" "system/framework/knoxsdk.jar" \
     "$MODPATH/knoxsdk.jar/0001-Introduce-KnoxPatchHooks.patch"
 
@@ -27,6 +30,7 @@ APPLY_PATCH "system" "system/framework/knoxsdk.jar" \
 SMALI_PATCH "system" "system/framework/samsungkeystoreutils.jar" \
     "smali/com/samsung/android/security/keystore/AttestParameterSpec.smali" "return" \
     'isVerifiableIntegrity()Z' 'true'
+
 APPLY_PATCH "system" "system/framework/services.jar" \
     "$MODPATH/services.jar/0001-Bypass-ICD-verification.patch"
 
@@ -45,7 +49,8 @@ SMALI_PATCH "system" "system/framework/services.jar" \
     "smali/com/android/server/StorageManagerService.smali" "return" \
     'isRootedDevice()Z' 'false'
 
-# Spoof ROT/IntegrityStatus in Knox Matrix
+# Spoof ROT/IntegrityStatus in Knox Matrix (Apenas se o KmxService existir na base)
+if [ -f "$WORK_DIR/system/system/priv-app/KmxService/KmxService.apk" ]; then
     SMALI_PATCH "system" "system/priv-app/KmxService/KmxService.apk" \
         "smali_classes2/com/samsung/android/kmxservice/common/util/RootOfTrust.smali" "return" \
         'getVerifiedBootState()I' '0'
@@ -79,4 +84,4 @@ SMALI_PATCH "system" "system/framework/services.jar" \
     SMALI_PATCH "system" "system/priv-app/KmxService/KmxService.apk" \
         "smali_classes2/com/samsung/android/kmxservice/sdk/trustchain/util/IntegrityStatus.smali" "return" \
         'isNormal()Z' 'true'
-
+fi
